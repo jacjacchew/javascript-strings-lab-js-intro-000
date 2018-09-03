3f20e209bad6026ea741ac3e09947263c4275964
Jaclyn Espinas <jacjacchew@gmail.com>
2018-09-03 22:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ var topic = "space";
 
 var conversation = `${topic}`;
 var greeting = "Hello, everybody!"
-var greeting = "Hello";
 var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
