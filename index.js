3b93584a57d1711c2cea5efc927fd07e50102eb4
Jaclyn Espinas <jacjacchew@gmail.com>
2018-09-03 22:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = 'Let\s talk about space';
+var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\s talk about + ${topic}`;
 var greeting = "Hello, everybody!"
 var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
